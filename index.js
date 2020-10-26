aabefd5c4be80c0fbd8bb5d4e6afa9e8b9bc80a4
zeyaoli <zeyao.li.315@gmail.com>
2020-10-26 11:03:52 -0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+import { pointsAlongLine } from "./vector.js";
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d", {
+  desynchronized: true,
+});
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 canvas.width = 800;
 canvas.height = 800;
 const { width, height } = canvas;
@@ -19,10 +25,29 @@ let px, py;
 
 let state = "blank";
 
-let blankText = document.getElementById("blank");
 let autumnBtn = document.getElementById("autumnLeaves");
 let dirtBtn = document.getElementById("dirt");
-let leavesBtn = document.getElementById("leaves");
+let chalkBtn = document.getElementById("chalk");
+
+//undo code from max
+
+let undoStack = [];
+pushState();
+function pushState() {
+  undoStack.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+
+  if (undoStack.length > 50) {
+    undoStack.shift();
+  }
+}
+
+document.getElementById("undo-btn").addEventListener("click", () => {
+  if (undoStack.length > 1) {
+    undoStack.pop();
+  }
+  let lastElement = undoStack[undoStack.length - 1];
+  ctx.putImageData(lastElement, 0, 0);
+});
 
 function resetButtonStyles() {
   Array.from(document.getElementsByClassName("tool-button")).forEach(
@@ -47,7 +72,6 @@ autumnBtn.addEventListener("click", () => {
     state = "blank";
   } else {
     state = "autumn";
-    blankText.style.display = "none";
     resetButtonStyles();
     activeBtn(autumnBtn);
   }
@@ -58,20 +82,18 @@ dirtBtn.addEventListener("click", () => {
     state = "blank";
   } else {
     state = "dirt";
-    blankText.style.display = "none";
     resetButtonStyles();
     activeBtn(dirtBtn);
   }
 });
 
-leavesBtn.addEventListener("click", () => {
-  if (state == "leaves") {
+chalkBtn.addEventListener("click", () => {
+  if (state == "chalk") {
     state = "blank";
   } else {
-    state = "leaves";
-    blankText.style.display = "none";
+    state = "chalk";
     resetButtonStyles();
-    activeBtn(leavesBtn);
+    activeBtn(chalkBtn);
   }
 });
 
@@ -81,8 +103,6 @@ document.getElementById("clear").addEventListener("click", () => {
   ctx.fillStyle = "rgb(251,247,242)";
   ctx.fillRect(0, 0, width, height);
   ctx.drawImage(bgImg, 0, 0, width, height);
-
-  blankText.style.display = "flex";
   resetButtonStyles();
 });
 
@@ -173,49 +193,21 @@ function dirtBrush(x, y) {
   }
 }
 
-function leaveBrush(x, y) {
-  let greenArr = [`hsl(120, 25%, 35%)`, `hsl(119, 30%, 53%)`];
-  ctx.beginPath();
-  //random chance - either two leaves or one
-  if (Math.random() < 0.5) {
-    ctx.ellipse(
-      x,
-      y - norm_random(5),
-      randomRange(25, 40),
-      randomRange(5, 15),
-      randomRange(Math.PI, Math.PI - 0.05),
-      0,
-      2 * Math.PI
-    );
-  } else {
-    for (let i = 0; i < 2; i++) {
-      ctx.ellipse(
-        x - norm_random(50),
-        y - norm_random(5),
-        randomRange(25, 40),
-        randomRange(5, 10),
-        randomRange(Math.PI + 0.5, Math.PI - 0.5),
-        0,
-        2 * Math.PI
-      );
-    }
-  }
-  // switch colors
-  if (Math.random() > 0.4) {
-    ctx.fillStyle = greenArr[0];
-  } else {
-    ctx.fillStyle = greenArr[1];
-  }
-  ctx.fill();
+function chalk(x, y) {
+  ctx.fillStyle = `rgb(255, 255, 255, .6)`;
+  let interpolatedPoints = pointsAlongLine(x, y, px, py, 0.05);
+  interpolatedPoints.forEach(function (p) {
+    ctx.fillRect(p.x + norm_random(30), p.y + norm_random(30), 1, 1);
+  });
 }
 
 function leaves(x, y) {
   let autumnPalette = [`#FF6034`, `#FF9828`, `#FFDB31`, `#D9D32F`];
   let eWidth = randomRange(25, 40);
   let eHeight = randomRange(5, 10);
-  ctx.translate(x + width / 2, y + height / 2); //translate origin
+  ctx.translate(x + randomRange(-20, 20), y + randomRange(-20, 20)); //translate origin
   ctx.rotate((Math.PI / 180) * randomRange(0, 360)); //rotate the image around origin
-  ctx.translate(-x - width / 2, -x - height / 2); //translate back
+  ctx.translate(-x - randomRange(-20, 20), -y - randomRange(-20, 20)); //translate back
 
   ctx.beginPath();
   ctx.ellipse(
@@ -249,20 +241,17 @@ function leaves(x, y) {
 
 function draw(x, y) {
   if (state == "autumn") {
-    autumnBrush(x, y);
+    if (distance(x, y, px, py) >= 25) {
+      leaves(x, y);
+    }
   } else if (state == "dirt") {
     dirtBrush(x, y);
-  } else if (state == "leaves") {
+  } else if (state == "chalk") {
     if (distance(x, y, px, py) >= 10) {
-      leaveBrush(x, y);
+      chalk(x, y);
     }
   } else {
-    // return;
-    leaves(x, y);
-
-    // if (distance(x, y, px, py) >= 5) {
-    //   leaves(x, y);
-    // }
+    return;
   }
 }
 
@@ -286,6 +275,7 @@ function paintMove(x, y) {
 
 function drawEnd(x, y) {
   penDown = false;
+  pushState();
 }
 
 canvas.addEventListener("mousedown", (event) => {
@@ -300,10 +290,12 @@ canvas.addEventListener("touchstart", (event) => {
   // console.log(touches);
   let touch = touches[0];
   let pos = getMousePos(canvas, touch);
-  console.log(touch);
+  // console.log(touch);
   let x = pos.x;
   let y = pos.y;
+  console.log(pos.x);
   drawStart(x, y);
+  console.log(canvas.width);
 });
 
 canvas.addEventListener("mousemove", (event) => {
@@ -378,4 +370,40 @@ canvas.addEventListener("touchend", (event) => {
 //   ctx.beginPath();
 //   ctx.arc(x, y, thickness / 2, 0, Math.PI * 2);
 //   ctx.fill();
+// }
+
+// function leavesBrush(x,y){
+// let greenArr = [`hsl(120, 25%, 35%)`, `hsl(119, 30%, 53%)`];
+// ctx.beginPath();
+// //random chance - either two leaves or one
+// if (Math.random() < 0.5) {
+//   ctx.ellipse(
+//     x,
+//     y - norm_random(5),
+//     randomRange(25, 40),
+//     randomRange(5, 15),
+//     randomRange(Math.PI, Math.PI - 0.05),
+//     0,
+//     2 * Math.PI
+//   );
+// } else {
+//   for (let i = 0; i < 2; i++) {
+//     ctx.ellipse(
+//       x - norm_random(50),
+//       y - norm_random(5),
+//       randomRange(25, 40),
+//       randomRange(5, 10),
+//       randomRange(Math.PI + 0.5, Math.PI - 0.5),
+//       0,
+//       2 * Math.PI
+//     );
+//   }
+// }
+// // switch colors
+// if (Math.random() > 0.4) {
+//   ctx.fillStyle = greenArr[0];
+// } else {
+//   ctx.fillStyle = greenArr[1];
+// }
+// ctx.fill();
 // }
